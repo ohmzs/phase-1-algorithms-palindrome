6c119513ca31b45847bc3cfdb1d98ec6a1a55140
ohmzs <38644134+ohmzs@users.noreply.github.com>
2022-06-07 14:52:24 -0400

    commit tree 6f31d451354dab87f3de41cb8df10a902dcfefe1

--- index.js
+++ index.js
@@ -1,9 +1,55 @@
+// function isPalindrome(word) {
+//   // Write your algorithm here
+// }
+
+// /* 
+//   Add your pseudocode here
+// */
+
+// /*
+//   Add written explanation of your solution here
+// */
+
+// // You can run `node index.js` to view these console logs
+// if (require.main === module) {
+//   // add your own custom tests in here
+//   console.log("Expecting: true");
+//   console.log("=>", isPalindrome("racecar"));
+
+//   console.log("");
+
+//   console.log("Expecting: false");
+//   console.log("=>", isPalindrome("robot"));
+// }
+
+// module.exports = isPalindrome;
+
+
+function reverse(word) {
+  // 'abc' => 'cba'
+  // const wordArray = word.split("");
+  // const reversedWordArray = wordArray.reverse();
+  // const reversedWord = reversedWordArray.join("");
+  // return reversedWord;
+  return word.split("").reverse().join("");
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = reverse(word);
+
+  return word === reversedWord;
 }
 
 /* 
   Add your pseudocode here
+  that means if the word is the same as the word in reverse, I should return true.
+  
+  reverse the input string
+
+  if the input is the same as the reversed input
+    return true
+  else
+    return false
 */
 
 /*
@@ -20,6 +66,85 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome(""));
 }
 
 module.exports = isPalindrome;
+
+
+//walkthrough sol 2 
+
+//function isPalindrome(word) {
+//   // iterate from the beginning to the middle
+//   for (let i = 0; i < word.length / 2; i++) {
+//     // check each letter to the correponding letter from the end
+//     const j = word.length - 1 - i;
+//     // if any letters don't match, return false
+//     if (word[i] !== word[j]) return false;
+//   }
+
+//   // return true
+//   return true;
+// }
+
+// /* 
+// r a c e c a r
+// 0 1 2 3 4 5 6
+// i           j
+
+// r a c e c a r
+// 0 1 2 3 4 5 6
+//   i       j
+
+// r a c e c a r
+// 0 1 2 3 4 5 6
+//     i   j
+
+// r a c e c a r
+// 0 1 2 3 4 5 6
+//       ij
+
+
+//   Add your pseudocode here
+//   That means if the
+//   first letter is the same as last letter, and the second letter is the same
+//   as the second to last letter, etc. until we reach the middle, return true.
+
+//   iterate from the beginning to the middle
+//     check each letter to the correponding letter from the end
+//       if any letters don't match, return false
+
+//   return true
+// */
+
+// /*
+//   Add written explanation of your solution here
+// */
+
+// // You can run `node index.js` to view these console logs
+// if (require.main === module) {
+//   // add your own custom tests in here
+//   console.log("Expecting: true");
+//   console.log("=>", isPalindrome("racecar"));
+
+//   console.log("");
+
+//   console.log("Expecting: false");
+//   console.log("=>", isPalindrome("robot"));
+
+//   console.log("");
+
+//   console.log("Expecting: true");
+//   console.log("=>", isPalindrome("abba"));
+// }
+
+// module.exports = isPalindrome;
